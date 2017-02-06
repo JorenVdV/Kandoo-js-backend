d8bdf3cedaf1b119058659f32530d2d9075a1ccc
Nick Jorens <nickjorens@gmail.com>
2017-02-06 10:42:05 +0100

    Added our first test using Mocha

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var app = express();
 
 require("./routes")(app);
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://146.185.153.213/teamjs');
 
 let server = require('http').Server(app);
 
