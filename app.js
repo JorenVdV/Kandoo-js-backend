39474cbda8d9705a1e0c041519c0d59a6bdd7c61
Steven Gentens <steven.gentens@student.kdg.be>
2017-02-06 09:29:15 +0100

    rename tests folder to test

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 let express = require('express'),
     path = require('path');
+
 var app = express();
+
 let server = require('http').Server(app);
 
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 8000;
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', function(req, res, next){
     res.sendStatus(200);
 });
+
 server.listen(port, function() {
     console.log("App is running on port " + port);
 });
