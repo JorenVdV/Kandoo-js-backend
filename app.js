8652a0f236d275ff62a5490fcd1266a1e36616f7
Steven Gentens <steven.gentens@student.kdg.be>
2017-03-07 12:39:39 +0100

    fix build

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(config.mongoURI[app.settings.env], config.options, function (er
 });
 
 //CORS
-app.user(cors());
+app.use(cors());
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
